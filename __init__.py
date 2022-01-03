22f40a7c91428623176dd68235b93c93efe22215
Elyashiv Danino <elyashiv3839@gmail.com>
2022-01-03 07:33:05 +0200

    First release

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from .compare_objects.CompareObjectsWithInfo import compare_objects_with_info
 from .compare_objects.CompareObjects import compare_objects
 
 __version__ = "1.0.0"
-__author__ = "daninoel@rafael.local"
+__author__ = "elyashiv3839"
